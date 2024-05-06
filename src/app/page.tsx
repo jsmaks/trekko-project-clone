5e9f026a5ede758f5f651e034d39fa600409b88d
jsmaks <jsmaks26@gmail.com>
2024-05-06 17:38:02 +0300

    add rep

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <header>
+      <div className="">
+        {' '}
+        <h1 className="text-2xl font-bold">Ебучее резюме</h1>
+        <div className='bg-red-200 w-[20%] h-full'>1</div>
+      </div>
+    </header>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      xl: '1280px',
+      smOnly: { max: '767.98px' },
+      mdOnly: { min: '768px', max: '1279.98px' },
+      notXl: { max: '1279.98px' },
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1.5rem',
+          sm: '1.5rem',
+          md: '2rem',
+          xl: '2rem',
+        },
+      },
+    },
+  },
+  // CONTAINER
+
+  plugins: [],
+};
+export default config;
