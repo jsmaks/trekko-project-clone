d05059e74f0c042deb4954c6a77961aa2bea7f55
jsmaks <jsmaks26@gmail.com>
2024-05-14 15:51:56 +0300

    fix

--- src/app/(auth)/select-org/[[...select-org]]/page.tsx
+++ src/app/(auth)/select-org/[[...select-org]]/page.tsx
@@ -1,20 +1,9 @@
-'use client';
+
 
 import { OrganizationList } from '@clerk/nextjs';
-import { useUser } from '@clerk/clerk-react';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 
 export default function CreateOrganizationPage() {
-  const { user } = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!user) {
-      router.push('/sign-in');
-    }
-  }, [user, router]);
 
   return (
     <div className="flex h-full items-center justify-center">
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isProtectedRoute = createRouteMatcher([
   '/',
   '/organization(.*)',
-  '/select-org',
+
 ]);
 
 export default clerkMiddleware((auth, request) => {
