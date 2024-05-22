1a898ce3d78b8129750349d6a879eebfbb0ebc31
jsmaks <jsmaks26@gmail.com>
2024-05-22 16:29:42 +0300

    add list form, list update

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { ListWithCards } from '@/types';
 import ListForm from './list-form';
+import ListItem from './list-item';
 
 interface ListContainerProps {
   data: ListWithCards[];
@@ -9,9 +11,25 @@ interface ListContainerProps {
 }
 //eslint-disable-next-line
 const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
   return (
     <div>
-      <ol>
+      <ol className="flex h-full gap-x-3">
+        {orderedData.map((list, index) => {
+          return (
+            <ListItem
+              key={list.id}
+              data={list}
+              index={index}
+              // boardId={boardId}
+            />
+          );
+        })}
         <ListForm />
         <div className="w-1 flex-shrink-0"></div>
       </ol>
