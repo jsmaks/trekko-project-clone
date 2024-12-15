878bab23330cb7fe2560ef6c8e16b1f5433f359b
jsmaks <jsmaks26@gmail.com>
2024-12-15 21:48:36 +0200

    fix boards

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,19 +11,35 @@ export default clerkMiddleware((auth, request) => {
   const { userId, orgId } = auth();
 
   // Если пользователь зарегистрирован (userId существует) и у него есть orgId
-  if (userId && orgId) {
-    // Проверяем, что пользователь не на странице /organization/${orgId}
-    if (!request.nextUrl.pathname.startsWith(`/organization/${orgId}`)) {
-      return NextResponse.redirect(
-        new URL(`/organization/${orgId}`, request.url)
-      );
-    }
+//  if (orgId && !request.nextUrl.pathname.startsWith(`/organization`)) {
+//    if (request.nextUrl.pathname === '/') {
+//      return NextResponse.redirect(
+//        new URL(`/organization/${orgId}`, request.url)
+//      );
+//    }
+
+
+//    // return NextResponse.redirect(
+//    //   new URL(`/organization/${orgId}`, request.url)
+//    // );
+//   }
+  if (
+    orgId &&
+    !request.nextUrl.pathname.startsWith(`/organization`) &&
+    !request.nextUrl.pathname.startsWith(`/board`)
+  ) {
+    return NextResponse.redirect(
+      new URL(`/organization/${orgId}`, request.url)
+    );
   }
+
 
   // Если пользователь зарегистрирован (userId существует), но нет orgId
   if (userId && !orgId && !request.nextUrl.pathname.startsWith('/select-org')) {
     return NextResponse.redirect(new URL('/select-org', request.url));
   }
+
+  
 
   // Защита маршрутов
   if (isProtectedRoute(request)) {
